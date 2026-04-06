dd284aa252adcb005257f50bbe7b5a6c829e38d6
trapti patel <trapti@digitalnightowl.in>
2026-04-06 12:32:42 +0530

    dist-missing-issue-solve

---
+++ src/modules/auth/auth.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IAdminUser extends Document {
+  username: string;
+  password: string;
+  email: string;
+  role: "admin" | "editor" | "manager";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const AdminSchema: Schema<IAdminUser> = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "editor", "manager"],
+      default: "editor",
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const AdminUser = mongoose.model<IAdminUser>(
+  "AdminUser",
+  AdminSchema
+);
